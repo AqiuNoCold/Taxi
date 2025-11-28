9b8cdaf76ccab96b6a598ce847d8c4575a2cdcdc
Sion703 <862336319@qq.com>
2025-11-28 18:34:25 +0800

    K-fold

--- utils.py
+++ utils.py
@@ -16,6 +16,25 @@ def haversine(lon1, lat1, lon2, lat2):
     c = 2 * np.arcsin(np.sqrt(a))
     return R * c
 
+def calculate_bearing(lat1, lon1, lat2, lon2):
+    """
+    计算两点之间的方位角（单位：度）
+    """
+    lat1, lon1, lat2, lon2 = map(np.radians, [lat1, lon1, lat2, lon2])
+    
+    # 计算经度差
+    dLon = lon2 - lon1
+    
+    # 计算方位角公式 (Bearing Formula)
+    y = np.sin(dLon) * np.cos(lat2)
+    x = np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * np.cos(lat2) * np.cos(dLon)
+    
+    # 计算弧度并转回角度
+    bearing = np.degrees(np.arctan2(y, x))
+    
+    # 将范围调整到 [0, 360)
+    return (bearing + 360) % 360
+
 def preprocess_copy(df):
     df_new = df.copy()
     
@@ -37,7 +56,7 @@ def preprocess_copy(df):
 
     # 速度 km/h
     df_new["speed_kmh"] = df_new["distance_km"] / (df_new["trip_duration"] / 3600)
-    df_new.loc[df_new["speed_kmh"] > 200, "speed_kmh"] = np.nan  # 异常速度设为 NaN
+    df_new.loc[df_new["speed_kmh"] > 160, "speed_kmh"] = np.nan  # 异常速度设为 NaN
     # 经纬度范围过滤
     lon_min, lon_max = -74.3, -73.7
     lat_min, lat_max = 40.5, 41.0
@@ -53,6 +72,11 @@ def preprocess_copy(df):
         (df_new["dropoff_latitude"] < lat_min) | (df_new["dropoff_latitude"] > lat_max),
         ["dropoff_longitude", "dropoff_latitude"]
     ] = np.nan
+
+    df_new["direction"] = calculate_bearing(
+        df_new["pickup_latitude"], df_new["pickup_longitude"],
+        df_new["dropoff_latitude"], df_new["dropoff_longitude"]
+    )
 
     # 去除 distance_km 为 0 或负值的异常
     df_new.loc[df_new["distance_km"] <= 0, "distance_km"] = np.nan
@@ -161,6 +185,18 @@ def plot_day_of_week(df):
     plt.savefig("./imgs/day_of_week_distribution.png", dpi=300)
     plt.show()
 
+def plot_direction(df):
+    direction = df["direction"]
+
+    plt.figure(figsize=(8,4))
+    sns.histplot(direction, bins=36, binrange=(0.5,360.5), discrete=True, kde=False, color="gold")
+    plt.title("Direction Distribution", fontsize=14)
+    plt.xlabel("Direction (°)", fontsize=12)
+    plt.ylabel("Count", fontsize=12)
+    plt.xticks(range(0,360,15))
+    plt.savefig("./imgs/direction_distribution.png", dpi=300)
+    plt.show()
+
 def visualize_all(df):
     plot_duration(df)
     plot_distance(df)
@@ -170,6 +206,7 @@ def visualize_all(df):
     plot_dropoff_location(df)
     plot_month(df)
     plot_day_of_week(df)
+    plot_direction(df)
 
 def feature_importance(df):
     # 处理后数据
@@ -177,7 +214,7 @@ def feature_importance(df):
 
     # 选择特征
     feature_cols = ["vendor_id", "passenger_count", "pickup_longitude", "pickup_latitude",
-                    "dropoff_longitude", "dropoff_latitude", "pickup_hour", "distance_km","month","day_of_week"]
+                    "dropoff_longitude", "dropoff_latitude", "pickup_hour", "distance_km","month","day_of_week","direction"]
     
     # 去除缺失
     valid_idx = df_new[feature_cols].dropna().index
